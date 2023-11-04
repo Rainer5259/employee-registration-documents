034ca62b74a3752664b207547117d974624a5821
Rainer <rainer_tribosk8@hotmail.com>
2023-11-04 19:51:18 -0300

    chore: add rules in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,9 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off'
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
+    '@typescript-eslint/no-extra-non-null-assertion': 'off',
+    'no-constant-condition': 'off'
   }
 }
